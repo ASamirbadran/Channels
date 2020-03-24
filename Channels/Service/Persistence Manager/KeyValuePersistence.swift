0a105e1fa283c0c661a7de091a0305f04d45756a
ahmed.samir <ahmed.samir@ibtikar.net.sa>
2020-03-24 09:46:00 +0200

    Structure Type: story #
    Work Done :
    ===========
    - Adding Persistence Manager

---
+++ Channels/Service/Persistence Manager/KeyValuePersistence.swift
@@ -0,0 +1,47 @@
+//
+//  KeyValuePersistence.swift
+//  Channels
+//
+//  Created by Ahmed Samir on 3/24/20.
+//  Copyright © 2020 Ibtikar. All rights reserved.
+//
+
+import Foundation
+
+public class PersistenceKeyBase {
+}
+
+#if DEBUG
+private struct Statics {
+    static var registeredKeys = Set<String>()
+}
+#endif
+
+public final class PersistenceKey<Type>: PersistenceKeyBase {
+    public let key: String
+    public let defaultValue: Type
+
+    public init(key: String, defaultValue: Type) {
+        self.key = key
+        self.defaultValue = defaultValue
+
+        #if DEBUG
+        if Statics.registeredKeys.contains(key) {
+            fatalError("PersistenceKey '\(key)' is registered multiple times")
+        }
+        Statics.registeredKeys.insert(key)
+        #endif
+    }
+
+    fileprivate init(_ key: String, _ defaultValue: Type) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+}
+
+public protocol KeyValuePersistence {
+
+    static func valueForKey<T>(_ key: PersistenceKey<T>) -> T
+    static func setValue<T>(_ value: T?, forKey key: PersistenceKey<T>)
+    static func removeValueForKey<T>(_ key: PersistenceKey<T?>)
+}
