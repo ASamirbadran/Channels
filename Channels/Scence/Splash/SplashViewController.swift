d18f53adda9dc03c0a9d481296781e28aafe7391
ahmed.samir <ahmed.samir@ibtikar.net.sa>
2020-04-06 10:56:37 +0200

    Spplash Type: story #
    Work Done :
    ===========
    -  using UIViewPropertyAnimator to enhance Animation

--- Channels/Scence/Splash/SplashViewController.swift
+++ Channels/Scence/Splash/SplashViewController.swift
@@ -14,6 +14,7 @@ class SplashViewController: BaseViewController {
     private var presenter: SplashPresenterProtocol?
 
     // MARK: - Private Variables
+    var animator: UIViewPropertyAnimator!
 
     // MARK: - Computed Variables
 
@@ -31,6 +32,23 @@ extension SplashViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad?()
+        animator = UIViewPropertyAnimator(duration: 2, curve: .easeInOut) { [unowned self, logoImageView] in
+            logoImageView?.transform = CGAffineTransform(rotationAngle: 0).scaledBy(x: 500, y: 500)
+        }
+        
+        
+        animator.addCompletion { position in
+            switch position {
+            case .start:
+                print("start")
+            case .current:
+                print("current")
+            case .end:
+                Container.setRootViewController(Container.getHomeScreen())
+            default:
+                break
+            }
+        }
         
         UIView.animateKeyframes(withDuration: 5, delay: 0, options: .calculationModeCubic, animations: {
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25) {
@@ -40,16 +58,16 @@ extension SplashViewController {
             UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
                 self.logoImageView.transform = CGAffineTransform(scaleX: 2, y: 2)
             }
-            
+
             UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.25) {
                 self.logoImageView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
             }
-            
+
             UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
-                self.logoImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.logoImageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
             }
         }, completion: { finished in
-            Container.setRootViewController(Container.getHomeScreen())
+            self.animator.startAnimation()
         })
     }
 }
